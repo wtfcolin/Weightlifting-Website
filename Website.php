116674f2431e2344fab934b6e030d5879756901f
MrDenbo <144271536+MrDenbo@users.noreply.github.com>
2023-12-04 12:56:56 -0600

    Update Website.php

--- Website.php
+++ Website.php
@@ -33,19 +33,18 @@
         $columns = array(); // define an array of column labels for a table header
 
         try {
-            switch ($mode) {
               case "login":
               // read login information
               $username = (isset($_POST["username"])) ? $_POST["username"] : "-1";
               $password = (isset($_POST["password"])) ? $_POST["password"] : "-1";
               // Define SQL statement
-              $sql = "SELECT * from `admin` where username=:username and password = :password ";
+              $sql = "SELECT * from `login` where username=:username and password = :password ";
 
               $parameters =[":username" =>$username, ":password"=>md5($password)];
               print_r($parameters);
 
               // Execute the SQL statement
-              $stm = $moviestore->prepare($sql);
+              $stm = $lifting-app->prepare($sql);
               $stm->execute($parameters);
               $result = $stm->fetch();
 
@@ -57,7 +56,6 @@
                   // You may save any other information. For example, the full name can be assigned to a session variable.
                   $_SESSION["name"] = $result["username"];
                   echo "<p>Logged in as {$_SESSION["name"]}</p>";
-                  echo "<h3>Welcome to Online Movie Club<h3>";
               } else {
                   echo "<p>Invalid user</p>";
                   // Display the login form
@@ -65,7 +63,36 @@
               }
               break;
 
+          case "createAccount":
+            // Retrieve the form data
+            $email = $_POST['username'];
+            $password = $_POST['password'];
+
+            // Implement your database connection here
+            // Make sure to use prepared statements to prevent SQL injection
+
+            // Your SQL query to insert the data into your database table
+            $sql = "INSERT INTO your_table_name (username, password) VALUES (:username, :password)";
+
+            // Execute the query using prepared statements
+            // (Assuming you're using PDO for database interactions)
+            // Replace 'your_connection_parameters' with your actual database connection parameters
+            $pdo = new PDO('mysql:host=localhost;dbname=lifting-app', 'your_username', 'your_password');
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':username', $email);
+            $stmt->bindParam(':password', $password);
+
+            if ($stmt->execute()) {
+                // If the query is successful, redirect to the login form
+                header('Location: loginform.php');
+                exit();
+            } else {
+                // Handle any error scenarios accordingly
+                echo "Error occurred while adding user to the database.";
             }
+
+
     }//end of switch statements
     ?>
 
@@ -76,8 +103,8 @@
         <div class="tab-navigation">
             <a href="Customize.html">Customize Workouts</a>
             <a href="Calendar.html">Calendar</a>
-            <a href="Website.html">Home</a>
-            <a href="Profile.html">Profile</a>
+            <a href="Website.php">Home</a>
+            <a href="Profile.php">Profile</a>
         </div>
         <div id="calendar">
             <!-- Calendar content will go here -->
