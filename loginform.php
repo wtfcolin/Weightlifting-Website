986e4b8769f0f4dd163c8ad73715199eb100e302
MrDenbo <144271536+MrDenbo@users.noreply.github.com>
2023-12-04 12:56:42 -0600

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -1,35 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+  <!-- Link to External CSS File -->
+  <link rel="stylesheet" href="WebsiteLook.css">
 </head>
-    <body>
-<style>
- .form-signin {
-            max-width: 330px;
-            padding: 15px;
-            margin: 0 auto;
-            color: #017572;
-         }
-    .button-row {
-        margin-top: 10px;
-    }
-</style>
+<body>
+    <div id = "login-page" class="container">
+        <form method="post" action="Website.php?mode=login" class="form-signin">
+            <h3>Sign in</h3>
+            <input type="text" class="form-control" name="username" placeholder="username" required autofocus><br>
+            <input type="password" class="form-control" name="password" placeholder="password" required>
+            <p class="button-row"><button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Login</button></p>
+        </form>
 
-<div class = "container">
-      
-    <form method="post" action="Website.php?mode=login" class="form-signin">
-        <h3>Sign in</h3>
-        <input type = "text" class = "form-control" 
-            name = "username" placeholder = "username" required autofocus></br>
-        <input type = "password" class = "form-control" name = "password" placeholder = "password" required>
-        <p class="button-row"><button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "login">Login</button></p>
-    </form>      
-</div>
-    </body>
+        <!-- Button to create account page -->
+        <p class="text-center">Don't have an account?</p>
+        <div class="text-center">
+            <a href="create_account.php" class="btn btn-outline-primary">Create Account</a>
+        </div>
+    </div>
+</body>
 </html>
