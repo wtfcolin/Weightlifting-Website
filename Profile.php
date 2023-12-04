e4d7d1b13a5daabd4cf1e7dc96aa5b734184bb3d
MrDenbo <144271536+MrDenbo@users.noreply.github.com>
2023-12-04 12:57:19 -0600

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile - Workout App</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="WebsiteLook.css">
     <style>
         body {
             background-color: #f8f9fa; /* Bootstrap default background color */
